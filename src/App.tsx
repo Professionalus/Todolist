60c6c62f4dc709f49ddb4954bcd46dc6aa1698a6
Susanszky Adam <susanszkyadam00201@gmail.com>
2025-02-07 00:26:44 +0100

    To do list task8

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import '@mantine/core/styles.css';
+import { Button } from "@mantine/core";
+import './App.css';
+
 export default function App() {
+
     return (
-        <>
-            <h1>Hello World!</h1>
-        </>
+        <div className={"to-do-wrapper"}>
+            <div className={"to-do-title"}>
+                New Item
+            </div>
+            <div className={"new-item-wrapper"}>
+                <input type="text"></input>
+                <Button>Add</Button>
+            </div>
+            <div className={"to-do-title"}>
+                 Todo List
+            </div>
+            <div className={"to-do-item"}>
+                <input type="checkbox"></input>
+                <h3>Test1</h3>
+                <Button>Delete</Button>
+            </div>
+            <div className={"to-do-item"}>
+                <input type="checkbox"></input>
+                <h3>test2</h3>
+                <Button>Delete</Button>
+            </div>
+        </div>
     );
 }
