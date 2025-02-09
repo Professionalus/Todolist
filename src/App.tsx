569a3b5f03d9d8b754d89de28f3a7c1dadbe3ce0
Susanszky Adam <susanszkyadam00201@gmail.com>
2025-02-09 21:00:44 +0100

    To do list Task18

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,76 @@
 import '@mantine/core/styles.css';
 import { Button, Checkbox, Input } from "@mantine/core";
 import './App.css';
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function App() {
+
+    interface toDoItem {
+        id: string,
+        name: string,
+        checked: boolean
+    }
+
+    const [newItem, setNewItem] = useState("");
+    const [toDoItems, setToDoItems] = useState<toDoItem[]>([]);
+
+    function handleNewItemSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        setToDoItems((currentToDoItems) => {
+            return [...currentToDoItems, {id: crypto.randomUUID(), name: newItem, checked: false}]
+        });
+        setNewItem("");
+    }
+
+    function handleNewItemInputChange(event: ChangeEvent<HTMLInputElement>) {
+        setNewItem(event.target.value);
+    }
+
+    function handleToDoItemCheckboxChange(id: string, event: ChangeEvent<HTMLInputElement>) {
+        setToDoItems((currentToDoItems) => {
+            return currentToDoItems.map(item =>
+                item.id === id ? {...item, checked: event.target.checked} : item)
+        });
+    }
 
     return (
         <div className={"to-do-wrapper"}>
             <div className={"to-do-title"}>
                 New Item
             </div>
-            <div className={"new-item-wrapper"}>
-                <Input size="md" placeholder="Irj"/>
-                <Button>Add</Button>
-            </div>
+            <form onSubmit={(event) => {handleNewItemSubmit(event)}} className={"new-item-wrapper"}>
+                <Input onChange={(event) => {handleNewItemInputChange(event)}}
+                       value={newItem}
+                       size="md"
+                       placeholder="Irj"/>
+                <Button type="submit">Add</Button>
+            </form>
             <div className={"to-do-title"}>
                 Todo List
             </div>
-            <div className={"to-do-item"}>
-                <Checkbox
-                    defaultChecked
-                    label="I agree to sell "
-                />
-                <Button>Delete</Button>
-            </div>
-            <div className={"to-do-item"}>
-                <Checkbox
-                    defaultChecked
-                    label="I agree to sell "
-                />
-                <Button>Delete</Button>
+            {toDoItems.length === 0 ? <div className={"to-do-item-is-empty"}>The list is empty!</div> : null}
+            <div className={"to-do-item-wrapper"}>
+                {toDoItems.map(item =>
+                    toDoItem(item)
+                )}
             </div>
         </div>
     );
+
+    function toDoItem(item: toDoItem) {
+        return (
+            <div className={"to-do-item"} key={item.id}>
+                <Checkbox onChange={(event) => handleToDoItemCheckboxChange(item.id, event)}
+                          color="green"
+                          variant="outline"
+                          label={item.name}
+                          checked={item.checked}
+                          classNames={{
+                              label: item.checked ? "item-completed" : ""
+                          }}
+                />
+                <Button variant="outline" color="red">Delete</Button>
+            </div>
+        );
+    }
 }
